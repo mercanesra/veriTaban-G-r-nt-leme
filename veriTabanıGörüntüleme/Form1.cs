a57671b1b16b0ab66e3790f5b9a78c8a53d18f7b
Esra Mercan <mercanesra1644gmail.com>
2023-07-14 14:51:56 +0300

    Add project files.

---
+++ veriTabanıGörüntüleme/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace veriTabanıGörüntüleme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("Data Source=LAPTOP-SINNOVD9;initial Catalog=Ögrenciler;Integrated Security=True");
+        private void verilerigöster()
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Select *from bilgiler", baglan);
+            SqlDataReader oku = komut.ExecuteReader();
+
+            while (oku.Read())
+            {
+                ListViewItem ekle = new ListViewItem();
+                ekle.Text = oku["adSoyad"].ToString();
+                ekle.SubItems.Add(oku["sehir"].ToString());
+                ekle.SubItems.Add(oku["okul"].ToString());
+                listView1.Items.Add(ekle);
+            }
+            baglan.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            verilerigöster();
+        }
+    }
+}
